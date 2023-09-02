294482414b1ab3663bf055b1e419a9f870d1c207
anshad <muhammedanshadmdi5@gmail.com>
2023-09-02 12:44:23 +0530

    tryess

--- NewsHund/settings.py
+++ NewsHund/settings.py
@@ -190,8 +190,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER=env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER=env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER='muhammedanshadmdi6@gmail.com'
+EMAIL_HOST_PASSWORD='xijympbwrbmqpfme'
 EMAIL_USE_TLS=True
 
 
