401a490777010e4af3fb7a63964b0497effd0564
anshad <muhammedanshadmdi5@gmail.com>
2023-09-02 13:15:15 +0530

    newssss

--- NewsHund/asgi.py
+++ NewsHund/asgi.py
@@ -1,16 +1,11 @@
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
+from user_side.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from user_side.routing import websocket_urlpatterns  # Import your WebSocket routing configuration
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsHund.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns  # Use your WebSocket routing configuration here
-        )
-    ),
+    "websocket": URLRouter(websocket_urlpatterns),
 })
