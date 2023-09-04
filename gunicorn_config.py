82cabe3ae080b43e787ef7ffe6c09005c7331d59
anshad <muhammedanshadmdi5@gmail.com>
2023-09-04 09:11:13 +0530

    sqllite

--- gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-import multiprocessing
-
-bind = "0.0.0.0:8000"  # Replace with your desired host and port
-workers = multiprocessing.cpu_count() * 2 + 1  # Adjust the number of workers as needed
-worker_class = "uvicorn.workers.UvicornWorker"  # Use the Uvicorn worker class
