fedeb59aa2389f6f29857619544150b58a293086
anshad <muhammedanshadmdi5@gmail.com>
2023-09-04 14:58:07 +0530

    static

--- NewsHund/settings.py
+++ NewsHund/settings.py
@@ -126,7 +126,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         
         'NAME': env('NAME'),           
-        'USER': env('USER'),      # Database user
+        'USER': env('DBUSER'),      # Database user
         'PASSWORD': env('PASSWORD'),  # Database user's password
         'HOST': env('HOST'),  # Hostname or IP address of the PostgreSQL server
         'PORT': '',                  # Leave empty for default PostgreSQL port (5432)
@@ -183,6 +183,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =os.path.join(BASE_DIR, 'static/assets')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
