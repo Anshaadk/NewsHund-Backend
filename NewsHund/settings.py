73c8859cf7d7e67ef98c36366bca8d5b5d6177a3
anshad <muhammedanshadmdi5@gmail.com>
2023-09-07 19:14:00 +0530

    staticnotfix

--- NewsHund/settings.py
+++ NewsHund/settings.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 CORS_ALLOWED_ORIGINS = env("CORS_ORIGINS", default="").split(",")
@@ -175,9 +174,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static/assets')
+STATIC_ROOT =os.path.join(BASE_DIR, './assets')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
