fdb94a187d34298c6faf234004084faa805f263c
anshad <muhammedanshadmdi5@gmail.com>
2023-09-18 13:18:39 +0530

    secretenv

--- NewsHund/settings.py
+++ NewsHund/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nngf6l3$yc+qk965*flzv+u=+am5fmo5=#q4ntu!2*r-wia$-7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
